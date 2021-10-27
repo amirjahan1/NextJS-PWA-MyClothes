fefe511c7ca5c08c111fd8bba8db9c9ce2ab0790
Amirjahan_1 <amirjahan454@gmail.com>
2021-10-27 12:49:07 +0330

    Start Project MY-CLOTHES

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+            <Html lang="fa">
+                <Head>
+                    <title>My-Clothes</title>
+                    <meta charSet="UTF-8"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <meta name="keywords" content="NextJS, React, Redux"/>
+                        <meta name="author" content="Amir Jahan"/>
+                    <meta name="description" content="My Clothes Ecommerce" />
+                    <link rel="icon" href="/Images/Title/TitleIcon.png" />
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
