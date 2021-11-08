bea9969add03293bf46c49bdb42885cdc2872221
Amirjahan_1 <amirjahan454@gmail.com>
2021-11-08 16:35:48 +0330

    *****

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 const path = require('path')
 const withPWA = require('next-pwa');
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    swSrc: 'service-worker.js',
-  },
+module.exports = ({
   images: {
     domains: ['dkstatics-public.digikala.com'],
     hostname : "dkstatics-public.digikala.com"
