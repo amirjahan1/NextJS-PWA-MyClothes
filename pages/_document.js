daef631fbbdd4e40aca254dba92688a006f6941f
Amir Jahan <74596803+amirjahan1@users.noreply.github.com>
2021-11-16 12:57:13 +0330

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,8 @@ class MyDocument extends Document {
                           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
                           integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
                           crossOrigin="anonymous" referrerPolicy="no-referrer"/>
+                              
+                                <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
                 </Head>
                 <body id="body">
 
